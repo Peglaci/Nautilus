3eaf04465cfd3743bdfe3e34f1301d0244888c1a
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-04-14 20:01:44 -0700

    Background Changes

--- scripts/background.lua
+++ scripts/background.lua
@@ -31,14 +31,27 @@ a.cave_background_fade_in = a.cave_background_fade_out:new {
 
 local fadein = a.cave_background_fade_in
 
+local function IsBackground()
+	return options.Nautilus_CaveBackground and options.Nautilus_CaveBackground.enabled and modApi:getCurrentTileset() == TILESET
+end
+
 local function HOOK_AddBackground(mission)
-  if options.Nautilus_CaveBackground and options.Nautilus_CaveBackground.enabled and modApi:getCurrentTileset() == TILESET then
+  if IsBackground() then
 		Board:AddAnimation(Point(0,0),"cave_background_fade_in",ANIM_NO_DELAY)
 		modApi:scheduleHook((fadein.Time*(fadein.NumFrames-1))*1000, function() -- minus 1 cause it uses one less frame
-			--customAnim:add(Point(0,0),"cave_background")
-			Board:AddAnimation(Point(0,0),"cave_background",ANIM_NO_DELAY) --THERE'S JUST NO FADE OUT TILL CUSTOMANIM IS FIXED
+			customAnim:add(Point(0,0),"cave_background")
+			--Board:AddAnimation(Point(0,0),"cave_background",ANIM_NO_DELAY) --THERE'S JUST NO FADE OUT TILL CUSTOMANIM IS FIXED
 		end)
   end
+end
+
+--If you exit the game before the game saves, the anim doens't get saved. This checks for that.
+local function HOOK_CheckBackground(mission)
+	LOG(mission)
+	if mission and IsBackground() and not customAnim:get(Point(0,0),"cave_background") then
+		LOG("inside")
+		customAnim:add(Point(0,0),"cave_background")
+	end
 end
 
 local function HOOK_RemoveBackground(mission)
@@ -53,6 +66,7 @@ local function EVENT_onModsLoaded()
   modApi:addTestMechEnteredHook(HOOK_AddBackground)
   modApi:addMissionStartHook(HOOK_AddBackground)
 	modApi:addMissionEndHook(HOOK_RemoveBackground)
+	modapiext.addGameLoadedHook(HOOK_CheckBackground)
 end
 
 modApi.events.onModsLoaded:subscribe(EVENT_onModsLoaded)
