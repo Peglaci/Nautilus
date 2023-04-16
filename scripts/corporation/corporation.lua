cde6faee9809c88faed4d5223337338c2d5be99d
Gabriel Guariglia Perez <52384978+Tatuzudo@users.noreply.github.com>
2023-04-16 18:50:40 -0300

    Updated Music

--- scripts/corporation/corporation.lua
+++ scripts/corporation/corporation.lua
@@ -7,10 +7,49 @@ corporation.Description = "Extending far below the surface of the earth, N.H.I. 
 corporation.Color = GL_Color(18,23,27)
 corporation.Map = { "/music/grass/map" } --Music for island
 corporation.Music = {
-	"/music/grass/combat_delta",
-	"/music/grass/combat_gamma",
-	"/music/sand/combat_guitar",
+	"/music/grass/combat_delta", 		--Relics
+	"/music/sand/combat_eastwood", 		--Cataclysm
+	"/music/snow/combat_new", 			--Plasmodia (AE)
+	"/music/acid/combat_detritus"		--A.C.I.D.
 }
 
 -- reference the pilot id created in pilot.lua
 corporation.Pilot = "Nautilus_pilot_id"
+
+----------------
+-- music list --
+----------------
+
+-- OST: https://benprunty.bandcamp.com/album/into-the-breach-soundtrack
+-- AE OST: https://benprunty.bandcamp.com/album/into-the-breach-advanced-edition-soundtrack
+
+-- Archive
+-- "/music/grass/map"				--Old Earth (Map)
+-- "/music/grass/combat_delta"		--Relics
+-- "/music/grass/combat_gamma"		--Antiquity Row
+-- "/music/sand/combat_guitar"		--Old War Machines
+-- "/music/grass/combat_new"		--Kai Miller (AE)
+
+-- R.S.T.
+-- "/music/sand/map"				--Red Sand (Map)
+-- "/music/sand/combat_western"		--The Blast Garden
+-- "/music/sand/combat_eastwood"	--Cataclysm
+-- "/music/sand/combat_montage"		--Rusting Hulks
+-- "/music/sand/combat_new"			--The Big One (AE)
+
+-- Pinnacle
+-- "/music/snow/map"				--Zenith
+-- "/music/snow/combat_ice"			--Pinnacle Robotics
+-- "/music/snow/combat_zimmer"		--Blitzkreig
+-- "/music/snow/combat_train"		--Rift Riders
+-- "/music/snow/combat_new"			--Plasmodia (AE)
+
+-- Detritus
+-- "/music/acid/map"				--Detritus
+-- "/music/acid/combat_synth"		--The Wasteland
+-- "/music/general/ominous_master"	--Reprocessing
+-- "/music/acid/combat_detritus"	--A.C.I.D.
+-- "/music/acid/combat_new"			--Mist Eaters(AE)
+
+
+-- Game:TriggerSound()
