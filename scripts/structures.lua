4e501a971ccaa6aa9e024012ab25f963cecbd85c
DogKisser <sabo.nicolas@gmail.com>
2023-05-26 13:44:17 -0400

    Update structures.lua
    New structures (bringing us up to five)

--- scripts/structures.lua
+++ scripts/structures.lua
@@ -10,6 +10,15 @@ CreateStructure{
 }
 
 CreateStructure{
+	Id = "Str_GasStorage",
+	Name = "Gas Storage",
+	Path = "img/structures/",
+	Image = "gasstorage",
+	ImageOffset = Point(-23,0),
+	Reward = REWARD_POWER
+}
+
+CreateStructure{
 	Id = "Str_ThermalPile",
 	Name = "Thermal Pile",
 	Path = "img/structures/",
@@ -23,6 +32,15 @@ CreateStructure{
 	Name = "Mine",
 	Path = "img/structures/",
 	Image = "mine",
+	ImageOffset = Point(-23,0),
+	Reward = REWARD_REP
+}
+
+CreateStructure{
+	Id = "Str_VentilationShaft",
+	Name = "Ventilation Shaft",
+	Path = "img/structures/",
+	Image = "ventilationshaft",
 	ImageOffset = Point(-23,0),
 	Reward = REWARD_REP
 }
