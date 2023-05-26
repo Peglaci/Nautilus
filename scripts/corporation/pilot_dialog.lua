29cf61057afaaf1fc28026164cc9e02f385fb2f6
Djinn <sabo.nicolas@gmail.com>
2023-05-26 10:35:04 -0400

    Corp Pilot Dialogue

--- scripts/corporation/pilot_dialog.lua
+++ scripts/corporation/pilot_dialog.lua
@@ -1,266 +1,79 @@
 return {
     -- Game States
-    Gamestart = {
-		"Meridia, do you read? This is #self_full, from another timeline! You won't believe what I've seen!",
-		"Back, once again. Third time's a charm... or is this the fourth?",
-		"Does a body good to see the world looking so pristine...ish."
+	Gamestart = {
+	"Do you read? This is #self_full, reporting from the depths of time! You won't believe what I've seen!",
+	"Back in action. Let's make this dig count!",
+	"Feels good to be back on solid ground again, where I belong."
 	},
-    FTL_Found = {
-		"Is anyone else seeing this? What IS that?"
+	FTL_Found = {
+	"Is anyone else seeing this? What in the depths is that?"
 	},
-    FTL_Start = {
-		"Is that a frickin' alien?"
+	FTL_Start = {
+	"By the boss's beard, is that a frickin' alien?"
 	},
-    Gamestart_PostVictory = {
-		"Listen up - this is #self_full speaking, and I've just come from the future. An alternate timeline, where we won!",
-		"Hold on, guys. I've done this before, and now I'll do it again!",
-		"How many more can we save?"
+	Gamestart_PostVictory = {
+	"Listen up, crew! This is #self_full speaking, and I've just hit the motherlode! We struck it rich!",
+	"Hold on tight, everyone. I've seen this play out before, and we're coming out on top!",
+	"How many more veins of ore can we unearth?"
 	},
-    Death_Revived = {
-		"...That was a close one.",
-		"Back on my feet."
+	Death_Revived = {
+	"...That was a close call.",
+	"Back on my feet and ready to drill!"
 	},
-    Death_Main = {
-		"Wait, let me-",
-		"I can do thi-"
+	Death_Main = {
+	"Wait, let me... cough",
+	"I can handle this... cough"
 	},
-    Death_Response = {
-		"Dammit, #main!",
-		"Another one down..."
+	Death_Response = {
+	"Dammit, #main! Get it together down there!",
+	"Another one down... We can't afford to lose any more!"
 	},
-    Death_Response_AI = {
-		"Hmm.",
-		"That's a shame!",
-		"Do we, uh, have a spare?"
+	Death_Response_AI = {
+	"Hmm. We're running low on crew members.",
+	"That's a shame! Time to recruit some fresh miners.",
+	"Do we, uh, have a spare body in storage?"
 	},
-    TimeTravel_Win = {
-		"Did we... do it? Did we win!?",
-		"All in a day's work."
+	TimeTravel_Win = {
+	"Did we strike gold!?",
+	"All in a day's work for the mining crew."
 	},
-    Gameover_Start = {
-		"I can't believe it... the world...",
-		"No..."
+	Gameover_Start = {
+	"I can't believe it... It's all caving in...",
+	"No... Our hard work, gone in an instant..."
 	},
-    Gameover_Response = {
-		"Let's try it again. And this time, we'll win."
-	},
-    
-    -- UI Barks
-    Upgrade_PowerWeapon = {
-		"I can work with this.",
-		"Connection established.",
-		"Looks fancy. Hope it works."
-	},
-    Upgrade_NoWeapon = {
-		"...I don't want to tell you how to do your job, but...",
-		"Shouldn't I have some kind of... weapon?",
-		"Uh, I don't want to be a bother, but..."
-	},
-    Upgrade_PowerGeneric = {
-		"Loving the new specs, Commander.",
-		"Purring like a kitten.",
-		"That'll do!"
-	},
-    
-    -- Mid-Battle
-    MissionStart = {
-		"Alright, I'll take that one over there.",
-		"Vek are the same all over, I guess.",
-		"Back in my own timeline, I swear I'd been here before. Oh well."
-	},
-    Mission_ResetTurn = {
-		"Take two!", "I'll never get used to that.",
-		"Localized breach successful, I guess.",
-		"I think I need a minute to get my head to stop spinning..."
-	},
-    MissionEnd_Dead = {
-		"That's all of em'.",
-		"Did we take them all down? Nice!",
-		"Good tactics, Commander!"
-	},
-    MissionEnd_Retreat = {
-		"Yeah, that's right, get out of here!",
-		"Thank goodness, I needed a breather.",
-		"Where do you think they even go?"
+	Gameover_Response = {
+	"Let's gather our tools and try again. We won't stop until we've carved our way to victory."
 	},
 
-    PodIncoming = {
-		"Look alive, incoming pod!",
-		"I never thought I'd see one of those again.",
-		"Incoming!"
+	Upgrade_PowerWeapon = {
+		"I can work miracles with this tool.",
+		"Connection established. Let's dig deep!",
+		"Looks like a sturdy piece. Hope it can withstand the pressure."
 	},
-    PodResponse = {
-		"It's tough enough to survive impact, but not trampling - look alive!",
-		"Let's go get that, quick!"
+	Upgrade_NoWeapon = {
+		"...I don't want to tell you how to do your job, but... a miner without a pickaxe?",
+		"Shouldn't I have some kind of drilling equipment?",
+		"Uh, I don't want to be a bother, but... can I have a tool, please?"
 	},
-    PodCollected_Self = {
-		"Got you!",
-		"Safe and sound.",
-		"I call dibs!"
-	},
-    PodDestroyed_Obs = {
-		"I think I'm going to be sick.",
-		"Hope that wasn't something important.",
-		"Oh well."
-	},
-    Secret_DeviceSeen_Mountain = {
-		"Does anyone else see that? What's in there?"
-	},
-    Secret_DeviceSeen_Ice = {
-		"Does anyone else see that? What's down there?"
-	},
-    Secret_DeviceUsed = {
-		"Uh, I think I activated it?"
+	Upgrade_PowerGeneric = {
+		"Loving the new gear, Boss.",
+		"Purring like a well-oiled drill.",
+		"That'll do! Let's put it to good use."
 	},
 
-    Secret_Arriving = {
-		"What's that now?"
+	MissionStart = {
+		"Alright, I'll dig my way through that vein over there.",
+		"Vek are like stubborn boulders. They won't stop us from reaching our goal.",
+		"Back in the fray, where I belong. Feels like home."
 	},
-    Emerge_Detected = {
-		"Incoming!",
-		"From below!",
-		"Not another one!"
+	Mission_ResetTurn = {
+		"Take two! Let's mine those Vek back where they came from.",
+		"I'll never get used to that time distortion.",
+		"Localized breach successful, I guess. Time to get back to work.",
+		"I think I need a minute to steady myself... these time jumps are disorienting."
 	},
-    Emerge_Success = {
-		"I have eyes on a new target.",
-		"Why couldn't you have stayed down?"
-	},
-    Emerge_FailedMech = {
-		"And stay down!",
-		"No! No. You stay down there.",
-		"You're not getting up here."
-	},
-    Emerge_FailedVek = {
-		"Well done.",
-		"Ha!",
-		"Does anyone else feel embarassed, watching that?"
-	},
-
-    -- Mech State
-    Mech_LowHealth = {
-		"Getting hot in here, guys...",
-		"#self_mech is showing multiple major alerts. Not good.",
-		"I hope we have a spare #self_mech at the hangar."
-	},
-    Mech_Webbed = {
-		"Well, I hate that.",
-		"This is way worse than walking into a cobweb.",
-		"I'm stuck!",
-		"A little help!?"
-	},
-    Mech_Shielded = {
-		"A wall of force...",
-		"I don't think we had these in my home timeline. Did we?",
-		"Bring it on, Vek!"
-	},
-    Mech_Repaired = {
-		"That should hold for now.",
-		"Not bad work, if I do say so myself.",
-		"That should do."
-	},
-    Pilot_Level_Self = {
-		"I think I just had an epiphany.",
-		"Oh! I get it now!",
-		"How long have I been fighting?"
-	},
-    Pilot_Level_Obs = {
-		"Hey, well done!",
-		"Good for you!"
-	},
-    Mech_ShieldDown = {
-		"Should my shield be fizzling out like that?",
-		"Shield's down, but #self_mech is doing great!"
-	},
-
-    -- Damage Done
-    Vek_Drown = {
-		"I'm glad they can't swim.",
-		"Look at them sink!",
-		"I'm getting second-hand embarassment."
-	},
-    Vek_Fall = {
-		"Down you go.",
-		"Have a nice trip!",
-		"See you next fall!"
-	},
-    Vek_Smoke = {
-		"Hard to see, pal?",
-		"You got smoked!",
-		"Smoking is bad for your health!"
-	},
-    Vek_Frozen = {
-		"Chill out.",
-		"Freeze!",
-		"Cool it!"
-	},
-    VekKilled_Self = {
-		"One down.",
-		"That's three hundred for me.",
-		"That's three hundred for me. Wait, maybe I lost count.",
-		"Another one bites the dust."
-	},
-    VekKilled_Obs = {
-		"Well done.",
-		"That's gotta hurt.",
-		"Another one down."
-	},
-    VekKilled_Vek = {
-		"Nice.",
-		"Not very smart, are they?",
-		"I hope I got that on camera."
-	},
-
-    DoubleVekKill_Self = {
-		"Bingo, bango.",
-		"Two down, courtesy of #self_full.",
-		"I hope you guys saw that."
-	},
-    DoubleVekKill_Obs = {
-		"I saw that. Kudos!",
-		"Nice!",
-		"Two at once!"
-	},
-    DoubleVekKill_Vek = {
-		"That was embarassing.",
-		"So much for them."
-	},
-
-    MntDestroyed_Self = {
-		"Uh, no one needed that mountain, right?",
-		"Hope no one was climbing that."
-	},
-    MntDestroyed_Obs = {
-		"Yeah, you show that mountain!"
-	},
-    MntDestroyed_Vek = {
-		"I'd rather a mountain go down than a building.",
-		"Eat that, rocks!"
-	},
-
-    PowerCritical = {
-		"Grid's not looking good.",
-		"Grid's bottoming out!",
-		"Dammit!"
-	},
-    Bldg_Destroyed_Self = {
-		"...I'm so sorry.",
-		"...I can't believe it.",
-		"I can't watch..."
-	},
-    Bldg_Destroyed_Obs = {
-		"Look where you're fighting!",
-		"Ah! Watch out!"
-	},
-    Bldg_Destroyed_Vek = {
-		"...I'll kill all of you.",
-		"Your kind will pay!",
-		"Not again!"
-	},
-    Bldg_Resisted = {
-		"Huh! Would you look at that.",
-		"Built strong, I guess.",
-		"Grid is holding tight!"
-	},
+	MissionEnd_Dead = {
+		"That's the last of 'em. The tunnel's clear!",
 	
 	-- Shared Mission Events
 	--[[Mission_Train_TrainStopped = {},
