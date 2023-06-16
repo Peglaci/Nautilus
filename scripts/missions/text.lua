3a44861b96a78450748f711177d05d570be3d942
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-06-16 13:37:24 -0700

    Fix text overrides

--- scripts/missions/text.lua
+++ scripts/missions/text.lua
@@ -1,36 +1,26 @@
-Global_Texts = {
+Global_Texts["TipTitle_Env_Nautilus_Crumbling"] = "Crumbling Cave"
+Global_Texts["TipText_Env_Nautilus_Crumbling"] = "The land slowly crumbles from the outside in."
 
-  TipTitle_Env_Nautilus_Crumbling = "Crumbling Cave",
-  TipText_Env_Nautilus_Crumbling = "The land slowly crumbles from the outside in.",
+Global_Texts["TipTitle_Env_Falling_Mountains"] = "Falling Mountains"
+Global_Texts["TipText_Env_Falling_Mountains"] = "Mountains fall from the sky, killing any unit it lands on."
 
-  TipTitle_Env_Falling_Mountains = "Falling Mountains",
-  TipText_Env_Falling_Mountains = "Mountains fall from the sky, killing any unit it lands on.",
+Global_Texts["TipTitle_Env_Nautilus_Tremors"] = "Tremors"
+Global_Texts["TipText_Env_Nautilus_Tremors"] = "Tremors cause cracks to form around a central point."
 
-  TipTitle_Env_Nautilus_Tremors = "Tremors",
-  TipText_Env_Nautilus_Tremors = "Tremors cause cracks to form around a central point.",
+TILE_TOOLTIPS["NAH_Crumbling"] = {"Crumbling Tile","This tile will crumble out of existence."}
 
-}
+TILE_TOOLTIPS["NAH_Falling_Mountain"] = {"Falling Mountain","A mountain will fall here, killing any unit under it."}
 
-TILE_TOOLTIPS = {
+TILE_TOOLTIPS["Nautilus_Incinerator"] = {"Incinerator","Ground units pushed in will be shred by the incinerator."}
 
-  NAH_Crumbling = {"Crumbling Tile","This tile will crumble out of existence."},
+--TILE_TOOLTIPS["NAH_Pressure_Plate_Text"] = {"Pressure Plate","Triggers security walls to pop up on all 4 sides. Sends other units flying."}
 
-  NAH_Dig_Site = {"Dig Site","Excavate the Vek on this spot with the Excavator."},
+TILE_TOOLTIPS["Nautilus_Spike_Text"] = {"Crystal Spikes", "Deals 1 Damage to units entering the tile."}
 
-  NAH_Falling_Mountain = {"Falling Mountain","A mountain will fall here, killing any unit under it."},
+TILE_TOOLTIPS["NAH_Blast_Charge_Unpowered"] = {"Unpowered Blast Charge", "Friendly units activate blast charges if they stand next to them at the end of your turn."}
 
-  Nautilus_Incinerator = {"Incinerator","Ground units pushed in will be shred by the incinerator."},
+TILE_TOOLTIPS["NAH_Blast_Charge_Powered"] = {"Powered Blast Charge", "Preparing to explode, destroying itself and all surrounding units."}
 
-  NAH_Pressure_Plate_Text = {"Pressure Plate","Triggers security walls to pop up on all 4 sides. Sends other units flying."},
+TILE_TOOLTIPS["NAH_Tremor"] = {"Tremor", "This tile will become cracked."}
 
-  Nautilus_Spike_Text = {"Crystal Spikes", "Deals 1 Damage to units entering the tile."},
-
-  NAH_Blast_Charge_Unpowered = {"Unpowered Blast Charge", "Friendly units activate blast charges if they stand next to them at the end of your turn."},
-
-  NAH_Blast_Charge_Powered = {"Powered Blast Charge", "Preparing to explode, destroying itself and all surrounding units."},
-
-  NAH_Tremor = {"Tremor", "This tile will become cracked."},
-
-  NAH_Minerals = {"Crystal Site","Run the Driller over this location to mine its crystal."},
-
-}
+TILE_TOOLTIPS["NAH_Minerals"] = {"Crystal Site","Run the Driller over this location to mine its crystal."}
