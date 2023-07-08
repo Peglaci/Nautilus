9605e8b7d1725ef55617be82a1ce44dd6c737206
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-07-07 22:05:52 -0700

    Add Ranged tags to ranged enemies.

--- scripts/missions/digging.lua
+++ scripts/missions/digging.lua
@@ -184,6 +184,7 @@ CrystalFirefly =
 		SkillList = { "FireflyAtk1" },
 		SoundLocation = "/enemy/firefly_soldier_1/",
 		DefaultTeam = TEAM_ENEMY,
+    Ranged = true,
 		--Portrait = "portraits/scorpion.png",
 	}
 AddPawn("CrystalFirefly")
@@ -197,6 +198,7 @@ CrystalScarab =
 		SkillList = { "ScarabAtk1" },
 		SoundLocation = "/enemy/scarab_1/",
 		DefaultTeam = TEAM_ENEMY,
+    Ranged = true,
 		--Portrait = "portraits/scorpion.png",
 	}
 AddPawn("CrystalScarab")
